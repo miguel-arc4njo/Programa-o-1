12d3cefa214466109a96ac5b491f5b7474a64b2a
aluno <aluno@lab01-13>
2026-03-02 09:04:42 -0300

    minhaPrimeiraAtualizacao

---
+++ Programacao01/src/main/java/com/mycompany/programacao01/Programacao01.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programacao01;
+
+/**
+ *
+ * @author aluno
+ */
+public class Programacao01 {
+
+    public static void main(String[] args) {
+        System.out.println("ola mundo!");
+        
+    }
+}
